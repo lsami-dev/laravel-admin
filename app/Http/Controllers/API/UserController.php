9dff5743a670be37206e075a6154fdda6a971403
unknown <lsami.dev@gmail.com>
2022-06-24 12:42:34 +0100

    Added Laravel Passport

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -10,6 +10,18 @@ use App\Models\User;
 class UserController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    
+    
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
